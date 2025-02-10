d2d988b04248ae53281b1ec9eb7ade69d2f66633
mgoenka@yahoo.com <7562036+mgk2014@users.noreply.github.com>
2025-02-09 18:45:27 -0800

    Initial EDA

---
+++ plot_helpers.py
@@ -0,0 +1,34 @@
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Helper function to create seaborn plots that share the same appearance attributes
+#   such as: heading, title, x-y labels, and placement of the legend
+# 
+def render_plot(ax, heading='', subtitle='', xlabel='', ylabel='', rotation=20, plotname='',displaybarlabel=True):
+    
+    plt.suptitle(heading, fontsize = 18)                        # main heading of the plot
+    plt.title(subtitle, fontsize = 12)                          # sub title of the plot
+    
+    ax.set(xlabel=xlabel, ylabel=ylabel)                        # x and y labels
+    
+    if displaybarlabel == True:
+        for i in ax.containers:
+            ax.bar_label(i,)                                    # display values on bars
+        
+    #ax.set_xticklabels(ax.get_xticklabels(), fontsize=8)
+        
+    for patch in ax.patches:                                    # remove bars that do not have any values
+        if patch.get_height() == 0: 
+            patch.set_visible(False)
+    
+    if ax.get_legend():                                         # if legend is present, set the location to upper right
+        sns.move_legend(ax, "upper left", bbox_to_anchor=(1,1))     
+        
+    ax.tick_params(rotation=rotation, axis='x')                 # rotate the labels
+    
+    if len(plotname) > 0:
+        filename = 'plots/' + plotname
+        ax.get_figure().savefig(filename, bbox_inches='tight')
+    
+
